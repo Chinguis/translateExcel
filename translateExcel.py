ff31b527fa2135da6c818a038063a57eefbb1938
Chinguis Umirbayev <chinguis.umirbayev@usequantum.com>
2025-01-02 21:22:13 -0500

    assemble row strings first, before starting API calls

--- translateExcel.py
+++ translateExcel.py
@@ -17,6 +17,12 @@ numCols = sheet.max_column
 print("Number of rows:", numRows)
 print("Number of columns:", numCols)
 
+rowStrs = []
+for row in sheet.values:
+    strs = [str(val) for val in row]
+    concat = "#".join(strs)
+    rowStrs.append(concat)
+
 confirm = input("Are you sure you want to proceed? Enter 'yes' to continue.\n")
 if confirm != "yes":
     exit()
@@ -41,35 +47,29 @@ def make_request(content: str):
             time.sleep(wait_time)
     raise Exception("Failed to make request")
 
-async def translateRow(i):
+async def translateRow(rowStr):
     loop = asyncio.get_event_loop()
-    row = []
-    for j in range(1, numCols + 1):
-        cell = sheet.cell(i, j)
-        row.append(cell.value if cell.value else "None")
-    rowStr = "#".join(row)
-    newRow = []
-    tries = 0
-    while len(newRow) != len(row) and tries < 5:
-        response = await loop.run_in_executor(None, make_request, rowStr)
-        newRowStr = response.choices[0].message.content
-        newRow = newRowStr.split("#")
-        tries += 1
-    for j in range(1, numCols + 1):
-        cell = sheet.cell(i, j)
-        if newRow[j - 1] != "None":
-            cell.value = newRow[j - 1]
-    sheet.row_dimensions[i].height = None
+    response = await loop.run_in_executor(None, make_request, rowStr)
+    translatedRowStr = response.choices[0].message.content
+    return translatedRowStr
 
 
 async def main():
     startTime = datetime.datetime.now()
 
-    tasks = [translateRow(i) for i in range(1, numRows + 1)]
-    await asyncio.gather(*tasks) #unpacking operator *
+    tasks = [translateRow(rowStr) for rowStr in rowStrs]
+    translatedRowStrs = await asyncio.gather(*tasks) #unpacking operator *
+    translatedRows = [rowStr.split("#") for rowStr in translatedRowStrs]
 
     endTime = datetime.datetime.now()
     print("Time taken:", endTime - startTime)
+
+    for i in range(len(translatedRows)):
+        for j in range(len(translatedRows[i])):
+            if translatedRows[i][j] != "None":
+                cell = sheet.cell(i+1, j+1) #cell coordinates are 1 indexed
+                cell.value = translatedRows[i][j]
+        sheet.row_dimensions[i+1].height = None #make spreadsheet rows stretch to fit data
 
     timeStr = str(endTime.year) + str(endTime.month) + str(endTime.day) + str(endTime.hour) + str(endTime.minute) + str(endTime.second)
     workbook.save(timeStr + fileName)
